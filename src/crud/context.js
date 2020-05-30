6d6e58d19a1baeb9f2cb8e850a93f690ed750003
Brian Heilman <brian.heilman@gmail.com>
2020-05-30 10:34:59 -0400

    Fleshing out controller

--- src/crud/context.js
+++ src/crud/context.js
@@ -6,6 +6,38 @@ class Context {
 		this.changes = {};
 	}
 
+	async hasPermission(/*permission*/){
+		return true;
+	}
+
+	// method
+
+	// params
+	getParam(name){
+		if (this.query && name in this.query){
+			return this.query[name];
+		} else {
+			return null;
+		}
+	}
+
+	// query
+	getQuery(name){
+		if (this.query && name in this.query){
+			return this.query[name];
+		} else {
+			return null;
+		}
+	}
+
+	async getContent(){
+		return {};
+	}
+
+	async getFile(){
+		return null;
+	}
+
 	addChange(model, action, datum){
 		let modelInfo = this.changes[model];
 
@@ -23,10 +55,6 @@ class Context {
 
 	getChanges(){
 		return this.changes;
-	}
-
-	async hasPermission(/*permission*/){
-		return true;
 	}
 }
 
--- src/crud/controller.js
+++ src/crud/controller.js
@@ -1,4 +1,9 @@
 
+const error = require('bmoor/src/lib/error.js');
+const {Config} = require('bmoor/src/lib/config.js');
+
+const config = new Config();
+
 // -- post
 // create => POST: ''
 
@@ -19,3 +24,94 @@
 // delete => DELETE: '/'+[id]
 // delete => DELETE: '/'+[id1,id2]
 // delete => DELETE: ''+query
+
+function runUpdate(ids, service, delta, ctx){
+	if (ids.length > 1){
+		return Promise.all(ids.map(
+			id => service.update(id, datum, ctx)
+		));
+	} else if (ids === 1){
+		return service.update(id, datum, ctx);
+	} else {
+		throw error.create('called update without id', {
+			code: 'CRUD_CONTROLLER_WRITE_ID',
+			type: 'warn',
+			status: 400
+		});
+	}
+}
+
+class Controller {
+	constructor(service, nexus, settings){
+		this.service = service;
+		this.nexus = nexus;
+		this.settings = settings;
+	}
+
+	async read(ctx){
+		if (ctx.method === 'get'){
+			if (ctx.query){
+				return this.service.query(ctx.query, ctx);
+			} else if (ctx.params){
+				const ids = ctx.getParam('id').split(',');
+
+				if (ids.length > 1){
+					return this.service.readMany(ids, ctx);
+				} else if (ids.length){
+					return this.service.read(ids, ctx)
+					.then(res => {
+						if (!res){
+							throw error.create('called read without result', {
+								code: 'CRUD_CONTROLLER_READ_ONE',
+								type: 'warn',
+								status: 404
+							});
+						}
+
+						return res;
+					});
+				} else {
+					throw error.create('called read without id', {
+						code: 'CRUD_CONTROLLER_READ_ID',
+						type: 'warn',
+						status: 400
+					});
+				}
+			} else {
+				return this.service.readAll(ctx);
+			}
+		} else {
+			throw error.create('called read with method '+ctx.method, {
+				code: 'CRUD_CONTROLLER_READ_UNAVAILABLE',
+				type: 'warn',
+				status: 405
+			});
+		}
+	}
+
+	async write(ctx){
+		const datum = await ctx.getContent();
+
+		if (ctx.method === 'POST'){
+			return this.service.create(datum, ctx);		
+		} else if (ctx.method === 'PUT'){
+			return runUpdate(ctx.getParam('id').split(','), this.service, datum, ctx);
+		} else if (ctx.method === 'PATCH'){
+			return runUpdate(ctx.getParam('id').split(','), this.service, datum, ctx);
+		} else {
+			throw error.create('called write with method '+ctx.method, {
+				code: 'CRUD_CONTROLLER_WRITE_UNAVAILABLE',
+				type: 'warn',
+				status: 405
+			});
+		}
+	}
+
+	async delete(ctx){
+
+	}
+}
+
+module.exports = {
+	Controller
+};
